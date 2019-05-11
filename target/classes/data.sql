bc1cb6c9e8e04d7992784e63ceb31c6cd7e67df9
Thomas Herbin <thomas.herbin68@gmail.com>
2019-05-11 14:25:07 +0200

    Spring Boot integration 2

---
+++ target/classes/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user VALUES(1, 'Thomas' , 'Herbin', 'thomas.herbin68@gmail.com', 22, 'MALE', TO_DATE('03/20/1997','DD/MM/YYYY'));
+INSERT INTO user(id, firstName, lastName, email,age,gender, birthday) VALUES(2, 'Nicolas' , 'Herbin', 'nicolas.herbin@gmail.com', 24, 'MALE', 09/06/1994);
+INSERT INTO user(id, firstName, lastName, email,age,gender, birthday) VALUES(3, 'François' , 'Herbin', 'herbin.gauchard@gmail.com', 58, 'MALE', 23/09/1961);
