b79d18ebeb52235f59b571036ffbc456553454e8
Thomas Herbin <thomas.herbin68@gmail.com>
2019-05-11 14:08:23 +0200

    Spring Boot integration

---
+++ src/main/java/com/lab/HomeExchange/HomeExchangeApplication.java
@@ -0,0 +1,19 @@
+package com.lab.HomeExchange;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class HomeExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeExchangeApplication.class, args);
+	}
+
+
+}
+
